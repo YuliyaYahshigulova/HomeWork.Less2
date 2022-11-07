16ba2084fb1aacd731c59269eb913390a271340e
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-07 13:16:29 +0700

    add Less2.Task15

---
+++ Less2.Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число от 1 до 7 включительно: ");
+int d = Convert.ToInt32(Console.ReadLine());
+int x = 7;
+
+if (d > x)
+{
+    Console.WriteLine("Ошибка!");
+}
+else
+{
+    if (x - 1 <= d && d <= x)
+    {
+        Console.WriteLine("Выходной день!");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день!");
+    }
+}
