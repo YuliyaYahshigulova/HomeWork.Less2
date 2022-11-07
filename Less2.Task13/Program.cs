63acc349404518d470b97c5824f61cb8ffe7d034
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-07 13:35:28 +0700

    add Less2.Task13

---
+++ Less2.Task13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x <= 99)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    while (x >= 999)
+    {
+        x = x / 10;
+    }
+    Console.Write("Третья цифра числа: ");
+    Console.WriteLine(x % 10);
+}
