44d100702703d1924cef413c23701ddca493d18a
YuliyaYahshigulova <lukyashkina_da@mail.ru>
2022-11-07 13:48:18 +0700

    add Less2.Task10

---
+++ Less2.Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (99 < a && a <= 999)
+{
+    Console.Write("Вторая цифра числа: ");
+    Console.WriteLine((a / 10) % 10);
+}
+else
+{
+    Console.WriteLine("Некорректное число!");
+}
